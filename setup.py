e5ef470ca00d9fe48ef66a7acf48d387daf9ed14
Sourav Das <sd309382@gmail.com>
2025-07-22 21:31:09 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='iris data version control using aws',
+    author='Sourav Das',
+    license='MIT',
+)
